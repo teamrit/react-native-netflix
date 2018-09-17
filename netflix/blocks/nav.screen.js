dc3e68c047e65107db854272e98003d0dc3d7c63
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-09-16 23:17:06 -0400

    remove logs

--- netflix/blocks/nav.screen.js
+++ netflix/blocks/nav.screen.js
@@ -6,7 +6,6 @@ import {WhiteText as SampleText} from "./white.text";
 export const NavScreen = ({ navigation, banner }) => (
     <ScrollView style={{flex: 1, backgroundColor: "#bada55"}}>
         <SafeAreaView forceInset={{ horizontal: 'always' }}>
-            {console.log()}
             <SampleText>{banner}</SampleText>
         </SafeAreaView>
         <StatusBar barStyle="default" />
