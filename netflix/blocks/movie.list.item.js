be4a93f9f673a59393dba83cb8e23539bfbf373c
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-09-16 23:15:15 -0400

    add movie list item component for flatlist

--- netflix/blocks/movie.list.item.js
+++ netflix/blocks/movie.list.item.js
@@ -1,3 +1,17 @@
 import React from 'react';
+import {Image, View} from "react-native";
+import {WhiteText} from "./white.text";
+import PropTypes from 'prop-types';
 
-const MovieList
+export const MovieListItem = ({item}) => (
+    <View style={{width: 100, height: 120}}>
+        <Image source="https://via.placeholder.com/150x350.png" style={{width: 150, height: 350}}/>
+        <WhiteText>{item.key}</WhiteText>
+    </View>
+);
+
+MovieListItem.propTypes = {
+    item: PropTypes.shape({
+        key: PropTypes.string.isRequired
+    }).isRequired
+};
