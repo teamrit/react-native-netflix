0fc63365ec198c09cd836b4f5f748586d160b4c9
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-09-16 23:16:35 -0400

    add method to show route name with space

--- netflix/blocks/nav.bar.js
+++ netflix/blocks/nav.bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity,TouchableNativeFeedback, View} from 'react-native';
 import {SafeAreaView} from "react-navigation";
 import {dark} from "../utils/styles";
 import {WhiteText as Text} from "./white.text";
@@ -32,16 +32,10 @@ export class CustomTabBar extends React.Component {
     const { routes, index } = navigation.state;
     return (
         <SafeAreaView style={[styles.tabContainer, dark]}>
-            <View style={{position: "absolute", margin: 0 , left: 0, right: 0, top: 0, bottom: 0, zIndex: 100, backgroundColor: "rgba(0,0,0,0.5)"
-                , width: 500, height: 250
-            }}>
-                <Text>BGHIUASHDsuh</Text>
-            </View>
             <TouchableOpacity onPress={()=>navigation.navigate("Home")}>
                 <Image source={require('../images/netflix-logo.png')} style={{width: 50, height: 40}} />
             </TouchableOpacity>
             {routes.map((route, indexOfRoute) => {
-                console.log(indexOfRoute, index, route.routeName);
                 if ((index === 0 || indexOfRoute === index) && route.routeName !== 'Home')
                 return (<TouchableOpacity
                     onPress={() => {
@@ -50,7 +44,7 @@ export class CustomTabBar extends React.Component {
                     style={styles.tab}
                     key={route.routeName}
                 >
-                    <Text style={{fontSize: 30}}>{getRouteName(route.routeName)}</Text>
+                    <Text>{getRouteName(route.routeName)}</Text>
                 </TouchableOpacity>)
             })}
         </SafeAreaView>
